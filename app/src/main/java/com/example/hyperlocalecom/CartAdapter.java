e03fda51e144d9a82cf10d77684d3f96be583d39
GajjarVaidehi <vedugajjar7101@gmail.com>
2022-02-18 18:40:58 +0530

    add new

---
+++ app/src/main/java/com/example/hyperlocalecom/CartAdapter.java
@@ -0,0 +1,115 @@
+package com.example.hyperlocalecom;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class CartAdapter extends RecyclerView.Adapter {
+
+    private List<CartItemModel> cartItemModelList;
+
+    public CartAdapter(List<CartItemModel> cartItemModelList) {
+        this.cartItemModelList = cartItemModelList;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        switch (cartItemModelList.get(position).getType()){
+            case 0:
+                return CartItemModel.CART_ITEM;
+            case 1:
+                return CartItemModel.TOTOAL_AMOUNT;
+            default:
+                return -1;
+        }
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        switch (viewType){
+            case CartItemModel.CART_ITEM:
+                View cartItemView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.cart_item_layout,viewGroup,false);
+                return new CartItemViewHolder(cartItemView);
+            case CartItemModel.TOTOAL_AMOUNT:
+                View cartTotalView = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.cart_total_amount_layout,viewGroup,false);
+                return new CartTotalAmountViewHolder(cartTotalView);
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return cartItemModelList.size();
+    }
+
+    class CartItemViewHolder extends RecyclerView.ViewHolder{
+
+        private ImageView productImage;
+        private TextView productTitle;
+        private TextView productPrice;
+        private TextView cuttedPrice;
+        private TextView offersApplied;
+        private TextView productQuantity;
+
+        public CartItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            productImage = itemView.findViewById(R.id.product_image);
+            productTitle = itemView.findViewById(R.id.product_title);
+            productPrice = itemView.findViewById(R.id.product_price);
+            cuttedPrice = itemView.findViewById(R.id.cutted_price);
+            offersApplied = itemView.findViewById(R.id.offers_applied);
+            productQuantity = itemView.findViewById(R.id.product_quantity);
+        }
+
+        private void setItemDetails(int resource, String title, String productPriceText, String cuttedPriceText, int offersAppliedNo){
+            productImage.setImageResource(resource);
+            productTitle.setText(title);
+            productPrice.setText(productPriceText);
+            cuttedPrice.setText(cuttedPriceText);
+            if(offersAppliedNo > 0){
+                offersApplied.setVisibility(View.VISIBLE);
+                offersApplied.setText(offersAppliedNo + "Offers applied");
+            }else{
+                offersApplied.setVisibility(View.INVISIBLE);
+            }
+        }
+    }
+
+    class CartTotalAmountViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView totalItems;
+        private TextView totalItemsPrice;
+        private TextView deliveryPrice;
+        private TextView totalAmount;
+        private TextView savedAmount;
+
+        public CartTotalAmountViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            totalItems = itemView.findViewById(R.id.total_items);
+            totalItemsPrice = itemView.findViewById(R.id.total_items_price);
+            deliveryPrice = itemView.findViewById(R.id.delivery_price);
+            totalAmount = itemView.findViewById(R.id.total_price);
+            savedAmount = itemView.findViewById(R.id.saved_amount);
+        }
+
+        private void setTotalAmount(){
+
+        }
+    }
+}
