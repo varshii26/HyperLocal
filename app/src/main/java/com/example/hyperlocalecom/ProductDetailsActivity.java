c6f2277dab8aa57343fa8d377b076954b44743fc
GajjarVaidehi <vedugajjar7101@gmail.com>
2022-02-25 13:03:15 +0530

    add new

--- app/src/main/java/com/example/hyperlocalecom/ProductDetailsActivity.java
+++ app/src/main/java/com/example/hyperlocalecom/ProductDetailsActivity.java
@@ -2,24 +2,33 @@ package com.example.hyperlocalecom;
 
 import static com.example.hyperlocalecom.MainActivity.showCart;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
+import android.media.Image;
 import android.os.Bundle;
 import android.service.controls.actions.FloatAction;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.hyperlocalecom.databinding.ActivityMainBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,6 +39,12 @@ public class ProductDetailsActivity extends AppCompatActivity {
     private ViewPager productImagesViewPager;
     private TabLayout viewpagerIndicator;
 
+    private TextView productTitle;
+    private TextView productPrice;
+    private TextView cuttedPrice;
+    private View codIndicator;
+    private TextView tvCodIndicator;
+
     private ViewPager productDetailsViewPager;
     private TabLayout productDetailsTablayout;
 
@@ -38,6 +53,9 @@ public class ProductDetailsActivity extends AppCompatActivity {
     private static boolean ALREADY_ADDED_TO_WISHLIST = false;
     private FloatingActionButton addToWishlistBtn;
 
+    private FirebaseFirestore firebaseFirestore;
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -52,21 +70,55 @@ public class ProductDetailsActivity extends AppCompatActivity {
         productImagesViewPager = (ViewPager)findViewById(R.id.product_images_viewpager);
         viewpagerIndicator = findViewById(R.id.viewpager_indicator);
         addToWishlistBtn = findViewById(R.id.add_to_wishlist_btn);
+        productTitle = findViewById(R.id.product_title);
+        productPrice = findViewById(R.id.product_price);
+        cuttedPrice = findViewById(R.id.cutted_price);
+        tvCodIndicator = findViewById(R.id.tv_cod_indicator);
+        codIndicator = findViewById(R.id.cod_indicator_imageview);
 
         productDetailsViewPager = findViewById(R.id.product_details_viewpager);
         productDetailsTablayout = findViewById(R.id.product_details_tablayout);
 
         buyNowBtn = findViewById(R.id.buy_now_btn);
 
-        List<Integer> productImages = new ArrayList<>();
-        productImages.add(R.drawable.assorted_pocket_diary_p4);
-        productImages.add(R.drawable.assorted_pocket_diary_1);
-        productImages.add(R.drawable.assorted_pocket_diary_2);
-        productImages.add(R.drawable.assorted_pocket_diary_3);
-        productImages.add(R.drawable.assorted_pocket_diary_4);
+        firebaseFirestore = FirebaseFirestore.getInstance();
 
-        ProductImagesAdapter productImagesAdapter = new ProductImagesAdapter(productImages);
-        productImagesViewPager.setAdapter(productImagesAdapter);
+        final List<String> productImages = new ArrayList<>();
+
+        firebaseFirestore.collection("PRODUCTS").document("HB9DxGYHfO0HLpbUTT2d")
+                .get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()){
+                    DocumentSnapshot documentSnapshot = task.getResult();
+
+                    for (long x = 1; x < (long)documentSnapshot.get("no_of_product_images") + 1 ; x++){
+                        productImages.add(documentSnapshot.get("product_image_"+x).toString());
+                    }
+                    ProductImagesAdapter productImagesAdapter = new ProductImagesAdapter(productImages);
+                    productImagesViewPager.setAdapter(productImagesAdapter);
+
+                    productTitle.setText(documentSnapshot.get("product_title").toString());
+                    productPrice.setText("Rs."+documentSnapshot.get("product_price").toString()+"/-");
+                    cuttedPrice.setText("Rs."+documentSnapshot.get("cutted_price").toString()+"/-");
+                    if ((boolean)documentSnapshot.get("COD")) {
+                        codIndicator.setVisibility(View.VISIBLE);
+                        tvCodIndicator.setVisibility(View.VISIBLE);
+                    }else {
+                        codIndicator.setVisibility(View.INVISIBLE);
+                        tvCodIndicator.setVisibility(View.INVISIBLE);
+                    }
+
+                    if ((boolean)documentSnapshot.get("use_tab_layout")){
+
+                    }
+
+                }else {
+                    String error = task.getException().getMessage();
+                    Toast.makeText(ProductDetailsActivity.this,error,Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         viewpagerIndicator.setupWithViewPager(productImagesViewPager,true);
 
